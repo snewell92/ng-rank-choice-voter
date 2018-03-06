3c5718f28e1d36ad1680b263a7902929630e47ba
Sean Newell <sean.newell@dorianbusiness.com>
2018-03-05 22:49:31 -0600

    Added basic (untested) support for hmr

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// TODO not sure why types fail here
+const bootstrap = platformBrowserDynamic().bootstrapModule(AppModule) as any;
+
+if (environment.hmr) {
+  if (module[ 'hot' ]) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap();
+}
